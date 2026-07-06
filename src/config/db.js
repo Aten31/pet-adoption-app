5403a1a033179c6e8f52e77aae27f78626d2199c
AadarshaMP <pandeyismyname@gmail.com>
2026-07-06 10:57:38 +0545

    minor changes

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
 
     await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("✅ MongoDB Connected");
+    console.log("MongoDB Connected");
     } catch (err) {
     console.error(err);
     }
